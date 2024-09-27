a0dd354cfd864057133a54dddde8a24b48ddb0ec
kalilakorin <kalila.korin@gmail.com>
2024-09-26 19:10:35 -0600

    remove commented out code

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var wmsSource = new ol.source.TileWMS({
-    // url: "https://ahocevar.com/geoserver/wms",
     url: "https://manifold.net/ims9/wms",
     params: { LAYERS: "map", TILED: true },
-    // serverType: "geoserver",
-    // crossOrigin: "anonymous"
     ratio: 1,
     transition: 0
 });
@@ -17,8 +14,6 @@ var map = new ol.Map({
     layers: [wmsLayer], 
     view: new ol.View({
         center: [-11721159.665362068, 4833266.172528265],
-        // center: ol.proj.fromLonLat([-105.4, 40]), 
-        // center: ol.proj.fromLonLat([-98.5795, 39.8283]),
         zoom: 5
     })
 });
